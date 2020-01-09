3d9ec2e745a01386c4330598cf35c3bc498e8c0c
vngarg <sngargrsd@gmail.com>
2020-01-09 16:26:52 +0530

    Completed Logistics Login and Signup

---
+++ logistics/templates/logistics/logistics.js
@@ -0,0 +1,8 @@
+function signup(){
+    document.getElementById("login").style.display="none"
+    document.getElementById("sign_up").style.display="block"
+}
+function login(){
+    document.getElementById("sign_up").style.display="none"
+    document.getElementById("login").style.display="block"
+}
