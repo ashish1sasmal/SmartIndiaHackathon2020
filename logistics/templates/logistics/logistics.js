56add6be52c5a3398a34104977cded59a7545500
vngarg <sngargrsd@gmail.com>
2020-01-09 20:08:53 +0530

    Login Modified

--- logistics/templates/logistics/logistics.js
+++ logistics/templates/logistics/logistics.js
@@ -6,13 +6,6 @@ function login(){
     document.getElementById("sign_up").style.display="none"
     document.getElementById("login").style.display="block"
 }
-function ask_pass(){
-    var username="Default User";
-    document.getElementById("sign_up").style.display="none"
-    document.getElementById("signup_final").style.display="block"
-    //assign variable "username" a value which will be used to display...
-    document.getElementById("user_span").innerHTML=username.bold()
-}
 function final_signup(){
     if(document.getElementById("password").value != document.getElementById("again password").value){
         document.getElementById("alert").style.display="block"
